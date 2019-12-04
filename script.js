6dba17715459354fe4c4bb31195ed06c2e89fbbe
Tarun M <tarunspartan1234@outlook.com>
2019-12-04 15:26:54 +0530

    added weather

--- script.js
+++ script.js
@@ -8,15 +8,19 @@ const clicked = (i) => {
             WelementIcon.classList.length ==3 ? WelementIcon.classList.remove(`${i}`) : WelementIcon.classList.add(`${i}`);
             break;
         case 'bluetooth':
+            let BAelementToggle = document.getElementById('bluetoothactive');
             let BelementToggle = document.getElementById(`${i}`);
             let BelementIcon = document.getElementById('i'+`${i}`);
-            BelementToggle.classList.length == 2 ? BelementToggle.classList.remove('on') : BelementToggle.classList.add('on');
+            BelementToggle.classList.length == 2 ? BelementToggle.classList.remove('on') & BAelementToggle.classList.remove('bluetoothactive') : BelementToggle.classList.add('on') & BAelementToggle.classList.add('bluetoothactive');
             BelementIcon.classList.length ==3 ? BelementIcon.classList.remove(`${i}`) : BelementIcon.classList.add(`${i}`);
             break;
         case 'airplane':
+            let AAelementToggle = document.getElementById('airplaneactive');
+            let SElement = document.getElementById('signal');
+            let SElement2 = document.getElementById('signal2');
             let AelementToggle = document.getElementById(`${i}`);
             let AelementIcon = document.getElementById('i'+`${i}`);
-            AelementToggle.classList.length == 2 ? AelementToggle.classList.remove('on') : AelementToggle.classList.add('on');
+            AelementToggle.classList.length == 2 ? AelementToggle.classList.remove('on') & AAelementToggle.classList.remove('airplaneactive') & SElement.classList.add('signal') & SElement2.classList.add('signal2') : AelementToggle.classList.add('on') & AAelementToggle.classList.add('airplaneactive') & SElement.classList.remove('signal') & SElement2.classList.remove('signal2');
             AelementIcon.classList.length ==3 ? AelementIcon.classList.remove(`${i}`) : AelementIcon.classList.add(`${i}`);
             break;
         case 'rotation':
@@ -26,9 +30,10 @@ const clicked = (i) => {
             RelementIcon.classList.length ==3 ? RelementIcon.classList.remove(`${i}`) : RelementIcon.classList.add(`${i}`);
             break;
         case 'location':
+            let LAelementToggle = document.getElementById('locationactive');
             let LelementToggle = document.getElementById(`${i}`);
             let LelementIcon = document.getElementById('i'+`${i}`);
-            LelementToggle.classList.length == 2 ? LelementToggle.classList.remove('on') : LelementToggle.classList.add('on');
+            LelementToggle.classList.length == 2 ? LelementToggle.classList.remove('on') & LAelementToggle.classList.remove('locationactive') : LelementToggle.classList.add('on') & LAelementToggle.classList.add('locationactive');
             LelementIcon.classList.length ==3 ? LelementIcon.classList.remove(`${i}`) : LelementIcon.classList.add(`${i}`);
             break;
         default:
